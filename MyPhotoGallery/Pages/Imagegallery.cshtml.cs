05158cdc795d90cad32c1bce921558f5bc3a45d8
Andri <andri.wakhid@mii.co.id>
2019-05-17 11:02:35 +0700

    commit

---
+++ MyPhotoGallery/Pages/Imagegallery.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyPhotoGallery.Models;
+
+namespace MyPhotoGallery.Pages
+{
+    public class ImageGalleryModel : PageModel
+    {
+        private ApplicationDbContext _context;
+        public List<GalleryImage> GalleryImages { get; set; }
+        public ImageGalleryModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task OnGet()
+        {
+            ViewData["SuccessMessage"] = TempData["SuccessMessage"];
+            GalleryImages = await _context.GalleryImages.ToListAsync();
+        }
+    }
+}
